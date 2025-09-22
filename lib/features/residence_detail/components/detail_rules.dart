888acf9369a1c3e19580531eaecca5cc1848e451
kitoko2 <dogbo804@gmail.com>
2025-09-22 23:11:25 +0000

    fix wording

--- lib/features/residence_detail/components/detail_rules.dart
+++ lib/features/residence_detail/components/detail_rules.dart
@@ -15,13 +15,13 @@ class DetailLogmentRules extends StatelessWidget {
             horizontalTitleGap: 0,
             leading: Icon(Icons.access_time),
             title:
-                Text("Heur d'arrivé a partir de: ${logmentModel.heureEntree}"),
+                Text("Heure d'arrivé a partir de: ${logmentModel.heureEntree}"),
           ),
         ),
         ListTile(
           horizontalTitleGap: 0,
           leading: Icon(Icons.access_time),
-          title: Text("Heur de départ avant : ${logmentModel.heureDepart}"),
+          title: Text("Heure de départ avant : ${logmentModel.heureDepart}"),
         ),
         Visibility(
           visible: !logmentModel.animauxAutorises!,
