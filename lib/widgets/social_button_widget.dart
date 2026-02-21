25a05df20039c05033baec95b78008cd4e6130ae
kitoko2 <dogbo804@gmail.com>
2026-02-21 15:02:42 +0000

    apple account integrate

--- lib/widgets/social_button_widget.dart
+++ lib/widgets/social_button_widget.dart
@@ -1,9 +1,9 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:immoplus_pro/cubits/authentification/login_cubit.dart';
 
 enum LoginMode {
@@ -47,27 +47,23 @@ class SocialLoginButtons extends StatelessWidget {
             },
             child: SvgPicture.asset(
               'assets/svgs/icons/google.svg',
-              width: 70,
+              width: 30,
             ),
           ),
 
           // Bouton Apple
-          // _SocialButton(
-          //   backgroundColor: Colors.black,
-          //   onPressed: () {
-          //     CustomPopup.toast(
-          //       text:
-          //           "La connexion via Apple n'est pas encore disponible pour le moment, mais elle le sera très bientôt.",
-          //       toastPosition: EasyLoadingToastPosition.bottom,
-          //       color: AppColors.primary,
-          //     );
-          //   },
-          //   child: const Icon(
-          //     FontAwesomeIcons.apple,
-          //     color: Colors.white,
-          //     size: 40,
-          //   ),
-          // ),
+          if (Platform.isIOS)
+            _SocialButton(
+              backgroundColor: Colors.black,
+              onPressed: () => context.read<LoginCubit>().signInWithApple(),
+              child: Center(
+                child: const Icon(
+                  FontAwesomeIcons.apple,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ),
+            ),
         ],
       ),
     );
