1f47b77dc4c1b7f76a36ea0af511d054e01fd2ed
kitoko2 <dogbo804@gmail.com>
2026-04-11 17:36:12 +0000

    update ui CreationDescriptionEditorV2

--- lib/common/widgets/v2/creation_description_editor_v2.dart
+++ lib/common/widgets/v2/creation_description_editor_v2.dart
@@ -31,34 +31,54 @@ class CreationDescriptionEditorV2 extends StatelessWidget {
           child: Column(
             children: [
               Container(
+                padding: const EdgeInsets.symmetric(vertical: 8),
                 decoration: BoxDecoration(
                   color: AppColors.primary,
-                  borderRadius: const BorderRadius.vertical(top: Radius.circular(14)),
+                  borderRadius:
+                      const BorderRadius.vertical(top: Radius.circular(14)),
                 ),
-                child: quill.QuillSimpleToolbar(
-                  controller: controller,
-                  config: const quill.QuillSimpleToolbarConfig(
-                    showSearchButton: false,
-                    showListCheck: false,
-                    showColorButton: false,
-                    showCodeBlock: false,
-                    showLink: false,
-                    showRedo: false,
-                    showSuperscript: false,
-                    showSubscript: false,
-                    showUnderLineButton: false,
-                    showBackgroundColorButton: false,
-                    toolbarSize: 40,
-                    showInlineCode: false,
-                    showFontSize: false,
-                    showFontFamily: false,
-                    showQuote: false,
-                    showHeaderStyle: false,
-                    showStrikeThrough: false,
-                    showClearFormat: false,
-                    iconTheme: quill.QuillIconTheme(
-                      iconButtonUnselectedData: quill.IconButtonData(color: Colors.white),
-                      iconButtonSelectedData: quill.IconButtonData(color: Colors.black87),
+                child: Theme(
+                  data: Theme.of(context).copyWith(
+                    iconTheme: const IconThemeData(color: Colors.white),
+                  ),
+                  child: quill.QuillSimpleToolbar(
+                    controller: controller,
+                    config: quill.QuillSimpleToolbarConfig(
+                      multiRowsDisplay: false,
+                      color: Colors.transparent,
+                      showSearchButton: false,
+                      showListCheck: false,
+                      showColorButton: false,
+                      showCodeBlock: false,
+                      showLink: false,
+                      showRedo: true,
+                      showSuperscript: false,
+                      showSubscript: false,
+                      showUnderLineButton: false,
+                      showBackgroundColorButton: false,
+                      toolbarIconAlignment: WrapAlignment.center,
+                      toolbarSize: 35,
+                      showInlineCode: false,
+                      showFontSize: false,
+                      showFontFamily: false,
+                      showQuote: false,
+                      showHeaderStyle: false,
+                      showStrikeThrough: false,
+                      showClearFormat: false,
+                      showDividers: false,
+                      showBoldButton: true,
+                      showItalicButton: true,
+                      showListNumbers: true,
+                      showListBullets: true,
+                      showAlignmentButtons: true,
+                      iconTheme: const quill.QuillIconTheme(
+                        iconButtonUnselectedData: quill.IconButtonData(
+                          color: Colors.white,
+                        ),
+                        iconButtonSelectedData: quill.IconButtonData(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -68,7 +88,8 @@ class CreationDescriptionEditorV2 extends StatelessWidget {
                 padding: const EdgeInsets.all(12),
                 decoration: const BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.vertical(bottom: Radius.circular(14)),
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(14)),
                 ),
                 child: quill.QuillEditor.basic(
                   controller: controller,
