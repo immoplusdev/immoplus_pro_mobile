a26ffe07d89e05b567a7a558ba8162a60798bc8b
marc0410 <koffikanmarcephrem@gmail.com>
2026-07-27 15:29:42 +0000

    feat: ajouter un indice de solde disponible dans le formulaire de retrait et améliorer les messages de succès de retrait

--- lib/features/payments/screen/withdraw_form_screen_v2.dart
+++ lib/features/payments/screen/withdraw_form_screen_v2.dart
@@ -19,6 +19,7 @@ import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:immoplus_pro/services/analytics_service.dart';
 import 'package:immoplus_pro/core/injection.dart';
 import 'package:immoplus_pro/app_states/request_state.dart';
+import 'package:immoplus_pro/utils/utils.dart';
 
 class WithdrawFormScreenV2 extends StatefulWidget {
   static const String name = 'withdraw_form_v2';
@@ -278,6 +279,8 @@ class _WithdrawFormScreenV2State extends State<WithdrawFormScreenV2> {
 
                         // Amount Custom Input Box (Standard TextFormField styled)
                         _buildAmountInputField(),
+                        const Gap(8),
+                        _buildAvailableBalanceHint(),
 
                         const Gap(40),
                       ],
@@ -479,6 +482,23 @@ class _WithdrawFormScreenV2State extends State<WithdrawFormScreenV2> {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildAvailableBalanceHint() {
+    return BlocBuilder<WalletCubit, RequestState>(
+      builder: (context, state) {
+        if (state is! WALLET) return const SizedBox.shrink();
+        final balance = state.data.availableBalance.toDouble();
+        return Text(
+          "NB : votre solde disponible est de ${Utils.formatCurrency(balance)}, vous pouvez retirer ce montant.",
+          style: GoogleFonts.sen(
+            fontSize: 12,
+            color: Colors.grey.shade500,
+            fontStyle: FontStyle.italic,
+          ),
+        );
+      },
     );
   }
 }
